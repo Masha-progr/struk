971ce985c330095454475bde582b580b5a30dd02
Maria <mirrastrukova@gmail.com>
2026-03-26 22:56:56 +0300

    Написали обработчик исключения для операции деления на ноль и сохранили сообщение об ошибке в файл log.txt

--- index.php
+++ index.php
@@ -1,26 +1,41 @@
 <?php
-function safe_fopen($filename, $rights) 
-{
-    $file = fopen($filename, $rights);
-    
-    if ($file === false) 
-    {
-        throw new ErrorException("Файл не удалось открыть");
+//function safe_fopen($filename, $rights) 
+//{
+//   $file = fopen($filename, $rights);
+//   if ($file === false) 
+//   {
+//       throw new ErrorException("Файл не удалось открыть");
+//   }    
+//   return $file;
+//}
+//try 
+//{
+//    $file = safe_fopen("nonexistent_file.txt", "r");
+//    echo "Файл открыт";
+//    fclose($file);
+//} 
+//catch (ErrorException $e) 
+//{
+//    echo "Сообщение: " . $e->getMessage() . "<br>";
+//    echo "Файл: " . $e->getFile() . "<br>";
+//    echo "Строка: " . $e->getLine();
+//}
+function safe_div($num, $div) {
+    if ($div == 0) {
+        throw new DivisionByZeroError("Делить на ноль нельзя!");
     }
-    
-    return $file;
+    return $num / $div;
 }
 
-try 
-{
-    $file = safe_fopen("nonexistent_file.txt", "r");
-    echo "Файл открыт";
-    fclose($file);
+try {
+    $result = safe_div(17, 0);
+    echo "$result <br>";
 } 
-catch (ErrorException $e) 
-{
-    echo "Сообщение: " . $e->getMessage() . "<br>";
-    echo "Файл: " . $e->getFile() . "<br>";
-    echo "Строка: " . $e->getLine();
+catch (DivisionByZeroError $e) {
+    $errMesg = $e->getMessage();
+    echo $errMesg . "<br>";
+    $filOp = fopen("log.txt", "a");
+    fputs($filOp, $errMesg . "<br>");
+    fclose($filOp);
 }
 ?>
