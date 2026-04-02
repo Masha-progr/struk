be9b7b65598ab36de6284a6931ba705b0dfb3aa3
Maria <mirrastrukova@gmail.com>
2026-04-02 11:16:56 +0300

    Создали класс Employee, добавили в него свойства и создали два объекта

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+class Employee{
+public $name;
+public $age;
+public $salary;
+
+public function __construct($name, $age, $salary) 
+{
+$this->name = $name;
+$this->age = $age;
+$this->salary = $salary;
+}
+}
+
+$emp1 = new Employee("Иван", 30, 50000);
+$emp2 = new Employee("Мария", 25, 60000);
+
+echo "{$emp1->name}, возраст: {$emp1->age}, зарплата: {$emp1->salary}<br>";
+echo "{$emp2->name}, возраст: {$emp2->age}, зарплата: {$emp2->salary}<br>";
+
+
+?>
