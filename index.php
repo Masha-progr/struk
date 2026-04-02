6b4a0ca9454d8e37801cd0d7963538df0e6efdd9
Maria <mirrastrukova@gmail.com>
2026-04-02 11:20:50 +0300

    Вывели сумму возрастов и сумму зарплат

--- index.php
+++ index.php
@@ -18,5 +18,6 @@ $emp2 = new Employee("Мария", 25, 60000);
 echo "{$emp1->name}, возраст: {$emp1->age}, зарплата: {$emp1->salary}<br>";
 echo "{$emp2->name}, возраст: {$emp2->age}, зарплата: {$emp2->salary}<br>";
 
-
+echo "Сумма зарплат: " . $emp1->salary + $emp2->salary . "<br>";
+echo "Сумма возрастов: " . $emp1->age + $emp2->age . "<br>";
 ?>
